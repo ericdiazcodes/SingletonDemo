f2b0cca43bf14b11735c6d7321295846a12992eb
Eric <ericdiazjr21@gmail.com>
2022-06-30 00:57:22 -0400

    add comments

--- src/variationjava/Main.java
+++ src/variationjava/Main.java
@@ -6,6 +6,7 @@ public class Main {
         var earth1 = Earth.getSingleton();
         var earth2 = Earth.getSingleton();
 
+        // This will print out the same object reference!
         System.out.println(earth1.toString());
         System.out.println(earth2.toString());
     }
--- src/variationkotlin/Earth.kt
+++ src/variationkotlin/Earth.kt
@@ -7,6 +7,7 @@ fun main() {
     val earth1 = Earth
     val earth2 = Earth
 
+    // This will print out the same object reference!
     println(earth1.toString())
     println(earth2.toString())
 }
